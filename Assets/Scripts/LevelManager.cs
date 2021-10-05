d5e0ec1cd47ddfdd108af4d38e25f702ed7fa27a
alperen <alperenerdogan0505@gmail.com>
2021-10-05 15:38:51 +0300

    Dynamically created score stairs added

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Utility;
 
 public class LevelManager : MonoBehaviour
 {
@@ -25,35 +26,6 @@ public class LevelManager : MonoBehaviour
     {
         currentLevel = Instantiate(levels[l-1], Vector3.zero, Quaternion.identity);
         currentLevel.transform.position += new Vector3(0, -10,10);
-        _scoreStairPoint = currentLevel.transform.Find("ScoreStairPoint").gameObject;
-        CreateScoreStair(30,_scoreStairPoint);
-    }
-    void CreateScoreStair(int stairNumber,GameObject scoreStairPoint)
-    {
-        scoreStairPoint.transform.position += scoreStairPoint.transform.localRotation*new Vector3(0, _offsetY / 2, _offsetZ / 2);
-        for (int i = 0; i < stairNumber; i++)
-        {
-            //score text cube
-            var scoreTextCubeInstantiated = Instantiate(scoreTextCube, Vector3.zero, Quaternion.identity, scoreStairPoint.transform);
-            scoreTextCubeInstantiated.transform.rotation = scoreStairPoint.transform.localRotation;
-            Vector3 textCubePos = new Vector3(0, i * _offsetY, i * _offsetZ);
-            scoreTextCubeInstantiated.transform.localPosition = textCubePos;
-            //set texts
-            var scoreTexts = scoreTextCubeInstantiated.GetComponentsInChildren<TextMesh>();
-            for (int j = 0; j < scoreTexts.Length; j++)
-            {
-                scoreTexts[j].text = "x" + i;
-            }
-            //score cube fill
-            var scoreCubeInstantiated = Instantiate(scoreCube, Vector3.zero, Quaternion.identity, scoreStairPoint.transform);
-            scoreCubeInstantiated.transform.rotation = scoreStairPoint.transform.localRotation;
-            var fillCubeScale = scoreCubeInstantiated.transform.localScale;
-            fillCubeScale.z *= (stairNumber - i);
-            // Vector3 fillCubePos = new Vector3(0, i * _offsetY, _offsetZ*(stairNumber -i)/2+(_offsetZ/2)+(i*_offsetZ));
-            Vector3 fillCubePos = new Vector3(0, i * _offsetY, _offsetZ*(stairNumber+i+1)/2);
-            scoreCubeInstantiated.transform.localScale = fillCubeScale;
-            scoreCubeInstantiated.transform.localPosition = fillCubePos;
-        }
     }
 
     public bool NextLevel()
---
+++ Assets/Scripts/Utility/ScoreStairCreator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Utility
+{
+    public class ScoreStairCreator : MonoBehaviour
+    {
+        public GameObject stairTextCube;
+        public GameObject stairFillCube;
+        private Vector3 _pos;
+        private Quaternion _rot;
+
+        private Vector3 _positionOffset;
+
+        //
+        public int numberOfStairs;
+
+        public float positionOffsetMultiplier; //scale difference for imports
+
+        //
+        private Vector2 _uvPos;
+        public Vector2 uvOffset;
+        public Color stairColor;
+
+        void Start()
+        {
+            _pos = Vector3.zero;
+            _rot = transform.localRotation;
+            _positionOffset = new Vector3(0, transform.localScale.y, transform.localScale.z) * positionOffsetMultiplier;
+            //
+            _uvPos = Vector2.zero;
+            //
+            CreateScoreStair(numberOfStairs);
+        }
+
+        void CreateScoreStair(int stairNumber)
+        {
+            //first offset will be half 
+            _pos += _pos + new Vector3(0, _positionOffset.y / 2, _positionOffset.z / 2);
+            for (int i = 0; i < stairNumber; i++)
+            {
+                var instantiated = Instantiate(stairTextCube, transform);
+                instantiated.transform.localPosition = _pos;
+                //position offset
+                _pos += _positionOffset;
+                //Creating new material and setting its own uv position
+                Material mat = new Material(Shader.Find("Specular"))
+                {
+                    mainTexture = stairTextCube.GetComponent<Renderer>().sharedMaterial.mainTexture,
+                    color = stairColor
+                };
+                mat.SetTextureOffset("_MainTex", _uvPos);
+                instantiated.GetComponent<Renderer>().material = mat;
+                // uv offset
+                _uvPos.x += uvOffset.x;
+       
+            }
+        }
+    }
+}
